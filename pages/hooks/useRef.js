3fc203f6a62096e64e90d1ba320801a7cae8038c
rizkymakruf <armppy8@gmail.com>
2022-09-12 20:44:16 +0700

    initial commit

--- pages/hooks/useRef.js
+++ pages/hooks/useRef.js
@@ -13,8 +13,9 @@ const Home = () => {
         <input
           ref={refer}
           type={"text"}
+          placeholder={"Type your text"}
           className={
-            "px-2 py-2 rounded-full focus:outline-none focus:ring-2 focus:ring-sky-300"
+            "px-3 py-2 rounded-full focus:outline-none focus:ring-2 focus:ring-sky-300"
           }
         />
         <button
@@ -26,8 +27,20 @@ const Home = () => {
           Get Value
         </button>
       </div>
-      <div className="w-full h-screen bg-gradient-to-br from-blue-300 to-purple-700 flex justify-center items-center">
-        TARO SINI CONTOHNYA
+      <div className="w-full h-screen bg-gradient-to-br from-blue-300 to-purple-700 flex justify-center items-center gap-3">
+        <input
+          ref={refer}
+          type={"file"}
+          className={"border-2 py-2 px-2 rounded-full bg-white"}
+        />
+        <button
+          onClick={() => console.log(refer.current.files[0])}
+          className={
+            "py-3 px-6 rounded-full bg-gradient-to-r from-orange-500 to-yellow-500 text-white font-bold"
+          }
+        >
+          Get Value
+        </button>
       </div>
     </>
   );
